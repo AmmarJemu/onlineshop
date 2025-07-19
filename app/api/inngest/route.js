cc33c1f7afe5e854781f3a6054b8375f08a74af1
Ammar Jemal <amarjemal6b@gmail.com>
2025-07-19 15:34:22 -0700

    Fixed the route jes inngest sync updation syn deletion

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,10 +1,7 @@
-import { inngest, syncUserCreation, syncUpdation, syncUserDeletion } from "@/config/inngest";
-import { serve } from "inngest";
+import { inngest } from "@/config/inngest";
 
-// Create the handler using the latest Inngest serve API
-export const { POST, GET, PUT } = serve({
-  client: inngest,
-  functions: [syncUserCreation, syncUpdation, syncUserDeletion],
+export const runtime = "edge";
 
-  
-});
+export async function POST(req) {
+  return inngest.handleRequest(req);
+}
