c17a0f542956a41ad12407cd76bcf68af2e63eeb
Ammar Jemal <amarjemal6b@gmail.com>
2025-07-20 10:04:39 -0700

    Fix middle

--- middleware.ts
+++ middleware.ts
@@ -1,32 +1,12 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-import { NextResponse } from 'next/server';
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
-// 1. Define public routes
-const isPublicRoute = createRouteMatcher([
-  '/',
-  '/sign-in(.*)',
-  '/sign-up(.*)',
-  '/api(.*)',
-  '/all-products(.*)'
-]);
-
-export default clerkMiddleware(async (auth, req) => {
-  // 2. Handle public routes
-  if (isPublicRoute(req)) {
-    return NextResponse.next();
-  }
-
-  // 3. Protect private routes
-  const { userId } = await auth();
-  if (!userId) {
-    const signInUrl = new URL('/sign-in', req.url);
-    signInUrl.searchParams.set('redirect_url', req.url);
-    return NextResponse.redirect(signInUrl);
-  }
-
-  return NextResponse.next();
-});
+export default clerkMiddleware()
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-};
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
